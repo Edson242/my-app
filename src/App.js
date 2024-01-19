d897a3f4bea43d724e5e26e18833565e4af889d4
Edson Silveira <edsonsilveira619@gmail.com>
2024-01-18 22:05:26 -0300

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Category from "./components/Category";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header/Index";
+import videos from "./json/videos.json";
+
+const categories = [
+  "Geografia",
+  "Como fazer e usar",
+  "Astronomia e Geografia",
+  "Climatologia, Meteorologia, Vegetação",
+  "Geologia e Hidrografia"
+]
+
+function filterCategorie(id){
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+      <Header />
+      <Banner image={'home'}/>
+      <Container>
+
+        <Category category={categories[0]}>
+          { filterCategorie(0).map(video => <Card id={video.id} key={video.id}></Card>) }
+        </Category>
+
+        <Category category={categories[1]}>
+          { filterCategorie(1).map(video => <Card id={video.id} key={video.id}></Card>) }
+        </Category>
+
+        <Category category={categories[2]}>
+          { filterCategorie(2).map(video => <Card id={video.id} key={video.id}></Card>) }
+        </Category>
+
+        <Category category={categories[3]}>
+          { filterCategorie(3).map(video => <Card id={video.id} key={video.id}></Card>) }
+        </Category>
+
+        <Category category={categories[4]}>
+          { filterCategorie(4).map(video => <Card id={video.id} key={video.id}></Card>) }
+        </Category>
+        
+      </Container>
+      <Footer />
+    </>
   );
 }
 
