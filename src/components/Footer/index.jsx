2bfc3d7c954b6b622a4c5db197dc1c32c6c147a5
Edson Silveira <edsonsilveira619@gmail.com>
2024-01-18 22:07:30 -0300

    feat: added basic components and construction of initial logic

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <h2>MovieFlix &copy; Desenvolvido por Edson Silveira <i>2023</i></h2>
+        </footer>
+    )
+}
+
+export default Footer;
